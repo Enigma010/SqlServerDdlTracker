0c3cd6506c2e6760727bad6a8780c25615ecdeb0
Enigma010 <jjodarsk@yahoo.com>
2017-05-14 20:56:00 -0500

    Adding DDL alter tracking and also cleaning up table definitions

--- SqlScripts/DdlAuditEvents.sql
+++ SqlScripts/DdlAuditEvents.sql
@@ -1,28 +1,44 @@
-CREATE TABLE DdlAuditEvents
-(
-    Id uniqueidentifier,
-    EventTime datetime,
-    LoginName varchar(150),
-    UserName varchar(150),
-    DatabaseName varchar(150),
-    SchemaName varchar(150),
-    ObjectName varchar(150),
-    ObjectType varchar(150),
-    Command varchar(max),
-    TriggerXml xml
-);
+SET ANSI_NULLS ON
+GO
 
-CREATE TABLE DdlAuditColumnEvents
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DdlAuditEvents](
+	[Id] [uniqueidentifier] NOT NULL,
+	[EventTime] [datetime] NULL,
+	[LoginName] [varchar](150) NULL,
+	[UserName] [varchar](150) NULL,
+	[DatabaseName] [varchar](150) NULL,
+	[SchemaName] [varchar](150) NULL,
+	[ObjectName] [varchar](150) NULL,
+	[ObjectType] [varchar](150) NULL,
+	[Command] [varchar](max) NULL,
+	[TriggerXml] [xml] NULL,
+PRIMARY KEY CLUSTERED 
 (
-    Id uniqueidentifier,
-    DdlAuditEventsId uniqueidentifier,
-    EventTime datetime,
-    LoginName varchar(150),
-    UserName varchar(150),
-    DatabaseName varchar(150),
-    SchemaName varchar(150),
-    ColumnName varchar(150),
-    ChangeType varchar(25),
-    Command varchar(max),
-    TriggerXml xml
-);
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[DdlAuditColumnEvents](
+	[Id] [uniqueidentifier] NOT NULL,
+	[DdlAuditEventsId] [uniqueidentifier] NOT NULL,
+	[EventTime] [datetime] NULL,
+	[LoginName] [varchar](150) NULL,
+	[UserName] [varchar](150) NULL,
+	[DatabaseName] [varchar](150) NULL,
+	[SchemaName] [varchar](150) NULL,
+	[ColumnName] [varchar](150) NULL,
+	[ChangeType] [varchar](25) NULL,
+	[Command] [varchar](max) NULL,
+	[TriggerXml] [xml] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
--- SqlScripts/DdlAuditEventsTrigger.sql
+++ SqlScripts/DdlAuditEventsTrigger.sql
@@ -76,3 +76,31 @@ create trigger DdlAuditEventsTrigger on database for CREATE_TABLE, ALTER_TABLE, 
     convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
     @event
   from @columnEvent.nodes('/AlterTableActionList/Create/Columns/Name') as DdlColumnCreateEvent(Col);
+
+  insert into [DdlAuditColumnEvents]
+  (
+    [Id],
+    [DdlAuditEventsId],
+    [EventTime],
+    [LoginName],
+    [UserName],
+    [DatabaseName],
+    [SchemaName],
+    [ColumnName],
+    [ChangeType],
+    [Command],
+    [TriggerXml]
+  )
+  select
+    NEWID(),
+    @id,
+    replace(convert(varchar(50), @event.query('data(/EVENT_INSTANCE/PostTime)')), 'T', ' '),
+    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/LoginName)')),
+    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/UserName)')),
+    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/DatabaseName)')),
+    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/SchemaName)')),
+    DdlColumnAlterEvent.Col.value('.', 'varchar(max)'),
+    'Alter',
+    convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
+    @event
+  from @columnEvent.nodes('/AlterTableActionList/Alter/Columns/Name') as DdlColumnAlterEvent(Col);  
