42658a15d1fba6e1cfeccdb7e4a4c40052a6060f
Enigma010 <jeremy.jodarski@sonicfoundry.net>
2017-05-16 13:53:59 -0500

    Adding dynamic SQL for the trigger

--- SqlScripts/DdlAuditEventsTrigger.sql
+++ SqlScripts/DdlAuditEventsTrigger.sql
@@ -1,27 +1,34 @@
-create trigger DdlAuditEventsTrigger on database for CREATE_TABLE, ALTER_TABLE, DROP_TABLE as
+declare @databaseName varchar(50)
+declare @sql nvarchar(max)
+
+set @databaseName = '{{AuditDatabaseName}}'
+
+set @sql =
+'create trigger [DdlAuditEventsTrigger] on database for CREATE_TABLE, ALTER_TABLE, DROP_TABLE as
   declare @event xml;
   declare @columnEvent xml;
   declare @id uniqueidentifier;
 
   set @id = NEWID();
   set @event = EVENTDATA();
-  set @columnEvent = @event.query('/EVENT_INSTANCE/AlterTableActionList')
-  insert into DdlAuditEvents
+  set @columnEvent = @event.query(''/EVENT_INSTANCE/AlterTableActionList'')
+
+  insert into [' + @databaseName + ']..[DdlAuditEvents]
   values
   (
     @id,
-    replace(convert(varchar(50), @event.query('data(/EVENT_INSTANCE/PostTime)')), 'T', ' '),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/LoginName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/UserName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/DatabaseName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/SchemaName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/ObjectName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/ObjectType)')),
-    convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
+    replace(convert(varchar(50), @event.query(''data(/EVENT_INSTANCE/PostTime)'')), ''T'', '' ''),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/LoginName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/UserName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/DatabaseName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/SchemaName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/ObjectName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/ObjectType)'')),
+    convert(varchar(max), @event.query(''data(/EVENT_INSTANCE/TSQLCommand/CommandText)'')),
     @event
   );
 
-  insert into [DdlAuditColumnEvents]
+  insert into [' + @databaseName + ']..[DdlAuditColumnEvents]
   (
     [Id],
     [DdlAuditEventsId],
@@ -38,18 +45,18 @@ create trigger DdlAuditEventsTrigger on database for CREATE_TABLE, ALTER_TABLE, 
   select
     NEWID(),
     @id,
-    replace(convert(varchar(50), @event.query('data(/EVENT_INSTANCE/PostTime)')), 'T', ' '),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/LoginName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/UserName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/DatabaseName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/SchemaName)')),
-    DdlColumnDeleteEvent.Col.value('.', 'varchar(max)'),
-    'Drop',
-    convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
+    replace(convert(varchar(50), @event.query(''data(/EVENT_INSTANCE/PostTime)'')), ''T'', '' ''),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/LoginName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/UserName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/DatabaseName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/SchemaName)'')),
+    DdlColumnDeleteEvent.Col.value(''.'', ''varchar(max)''),
+    ''Drop'',
+    convert(varchar(max), @event.query(''data(/EVENT_INSTANCE/TSQLCommand/CommandText)'')),
     @event
-  from @columnEvent.nodes('/AlterTableActionList/Drop/Columns/Name') as DdlColumnDeleteEvent(Col);
+  from @columnEvent.nodes(''/AlterTableActionList/Drop/Columns/Name'') as DdlColumnDeleteEvent(Col);
 
-  insert into [DdlAuditColumnEvents]
+  insert into [' + @databaseName + ']..[DdlAuditColumnEvents]
   (
     [Id],
     [DdlAuditEventsId],
@@ -66,18 +73,18 @@ create trigger DdlAuditEventsTrigger on database for CREATE_TABLE, ALTER_TABLE, 
   select
     NEWID(),
     @id,
-    replace(convert(varchar(50), @event.query('data(/EVENT_INSTANCE/PostTime)')), 'T', ' '),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/LoginName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/UserName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/DatabaseName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/SchemaName)')),
-    DdlColumnCreateEvent.Col.value('.', 'varchar(max)'),
-    'Create',
-    convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
+    replace(convert(varchar(50), @event.query(''data(/EVENT_INSTANCE/PostTime)'')), ''T'', '' ''),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/LoginName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/UserName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/DatabaseName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/SchemaName)'')),
+    DdlColumnCreateEvent.Col.value(''.'', ''varchar(max)''),
+    ''Create'',
+    convert(varchar(max), @event.query(''data(/EVENT_INSTANCE/TSQLCommand/CommandText)'')),
     @event
-  from @columnEvent.nodes('/AlterTableActionList/Create/Columns/Name') as DdlColumnCreateEvent(Col);
+  from @columnEvent.nodes(''/AlterTableActionList/Create/Columns/Name'') as DdlColumnCreateEvent(Col);
 
-  insert into [DdlAuditColumnEvents]
+  insert into [' + @databaseName + ']..[DdlAuditColumnEvents]
   (
     [Id],
     [DdlAuditEventsId],
@@ -94,13 +101,15 @@ create trigger DdlAuditEventsTrigger on database for CREATE_TABLE, ALTER_TABLE, 
   select
     NEWID(),
     @id,
-    replace(convert(varchar(50), @event.query('data(/EVENT_INSTANCE/PostTime)')), 'T', ' '),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/LoginName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/UserName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/DatabaseName)')),
-    convert(varchar(150), @event.query('data(/EVENT_INSTANCE/SchemaName)')),
-    DdlColumnAlterEvent.Col.value('.', 'varchar(max)'),
-    'Alter',
-    convert(varchar(max), @event.query('data(/EVENT_INSTANCE/TSQLCommand/CommandText)')),
+    replace(convert(varchar(50), @event.query(''data(/EVENT_INSTANCE/PostTime)'')), ''T'', '' ''),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/LoginName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/UserName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/DatabaseName)'')),
+    convert(varchar(150), @event.query(''data(/EVENT_INSTANCE/SchemaName)'')),
+    DdlColumnAlterEvent.Col.value(''.'', ''varchar(max)''),
+    ''Alter'',
+    convert(varchar(max), @event.query(''data(/EVENT_INSTANCE/TSQLCommand/CommandText)'')),
     @event
-  from @columnEvent.nodes('/AlterTableActionList/Alter/Columns/Name') as DdlColumnAlterEvent(Col);  
+  from @columnEvent.nodes(''/AlterTableActionList/Alter/Columns/Name'') as DdlColumnAlterEvent(Col);'
+
+exec sp_executesql @sql
